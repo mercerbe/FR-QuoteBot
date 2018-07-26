1427db0d81b67ed368af7b006e7b1e7827b6cf5a
Admin <mercerbe@gmail.com>
2018-07-26 13:39:07 -0400

    mvp for heroku

---
+++ bot.js
@@ -0,0 +1,25 @@
+//dependencies
+const Twit = require('twit')
+const TwitterBot = require('node-twitterbot').TwitterBot
+require('dotenv').config()
+
+//config twitter app access
+var Bot = new TwitterBot({
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+})
+
+//quotes
+const quotesArray = require('./quotes.js')
+
+//pick random quote from array
+function pickQuote(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+const quote = pickQuote(quotesArray)
+
+//tweet picked quote
+Bot.tweet(quote)
